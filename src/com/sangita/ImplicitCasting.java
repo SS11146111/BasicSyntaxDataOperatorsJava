519f382688f0714f675fd596145bd1ac0844355c
SS11146111 <sahasangita59@gmail.com>
2025-04-03 13:54:30 +0530

    c4

---
+++ src/com/sangita/ImplicitCasting.java
@@ -0,0 +1,31 @@
+package com.sangita;
+
+import java.util.Scanner;
+
+public class ImplicitCasting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner scanner = new Scanner(System.in);
+		String flag= "y";
+		
+		while(flag.equals("y"))
+		{
+			
+			System.out.println("\nEnter an integer number: ");
+			int t1 = scanner.nextInt();
+			
+			double d = t1;
+		    
+			System.out.println("\n\nDouble number = " + d);25
+			
+			System.out.println("\nDo you wish to continue? y/n");
+			flag=scanner.next();
+			
+		}
+		
+		scanner.close();
+	}
+
+}
